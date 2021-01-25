3e28bd3cb3b324ed636cb9eea4da651dc44bf804
roybaryosef-netapp <77723231+roybaryosef-netapp@users.noreply.github.com>
2021-01-25 17:27:06 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_providers {
+    spotinst = {
+      source = "spotinst/spotinst"
+      version = "1.33.0"
+    }
+  }
+}
+
+
+provider "spotinst" {
+  token   = "b04f3efa399608aa79d29de47779cff8f29d5878fbe0c02841e51b5684801b33"
+  account = "act-54c5d1ab"
+}
+
+resource "spotinst_elastigroup_aws" "Terraform" {
+  name = "Roy Terraform Git"
+
+  spot_percentage = 100
+
+  orientation = "balanced"
+
+  draining_timeout = 120
+
+  fallback_to_ondemand = true
+
+  revert_to_spot {
+    perform_at = "always"
+  }
+
+  desired_capacity = 2
+
+  min_size = 1
+
+  max_size = 3
+
+  capacity_unit = "instance"
+
+  instance_types_ondemand = "t2.micro"
+
+  instance_types_spot = ["t2.micro", "t2.small", "t3.micro", "t3.small"]
+
+  subnet_ids = ["subnet-2e2e2465", "subnet-db7990a3", "subnet-ed8f74b0", "subnet-60a6d54b"]
+
+  product = "Linux/UNIX"
+
+  security_groups = ["sg-07648da43a33fe6b8"]
+
+  enable_monitoring = false
+
+  ebs_optimized = false
+
+  image_id = "ami-082b5a644766e0e6f"
+
+  key_name = "Roy_key"
+
+  placement_tenancy = "default"
+
+  region = "us-west-2"
+}
