2621d6e75acfc57c67e0214869d836e8c67b1cf7
roybaryosef-netapp <77723231+roybaryosef-netapp@users.noreply.github.com>
2021-02-01 11:45:21 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
-variable "spot_account_id" {
+variable "spotinst_account" {
   type    = string
   default = "act-54c5d1ab"
 }
 
-variable "spot_token" {
+variable "spotinst_token" {
   type    = string
   default = "b04f3efa399608aa79d29de47779cff8f29d5878fbe0c02841e51b5684801b33"
 }
